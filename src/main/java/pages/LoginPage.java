5939fc11849782b7144619fa3729eaaf793360bd
Aggarwal <50024558@acsind.com>
2021-01-25 04:30:18 +0530

    minor

--- src/main/java/pages/LoginPage.java
+++ src/main/java/pages/LoginPage.java
@@ -11,7 +11,7 @@ public class LoginPage {
 	private By emailID=By.id("email");
 	private By password=By.id("passwd");
 	private By signInButton=By.id("SubmitLogin");
-	private By forgotPwdLink=By.linkText("Forgot your password?!!");
+	private By forgotPwdLink=By.linkText("Forgot your password?");
 	
 	
 	
